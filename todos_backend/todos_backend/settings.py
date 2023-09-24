6511a48dbd94cdba0845be473d53c4e26a1c2cf1
Warw1ck <bradvata15@gmail.com>
2023-09-24 21:36:36 +0300

    Version 3

--- todos_backend/todos_backend/settings.py
+++ todos_backend/todos_backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,9 +43,13 @@ INSTALLED_APPS = [
 
     # Others
     'rest_framework',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,7 +57,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True  # Allow requests from any origin (not recommended for production)
+
 
 ROOT_URLCONF = 'todos_backend.urls'
 
