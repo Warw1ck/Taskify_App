fc125dbe242e994cbcac128ffa14b578691fae24
Warw1ck <bradvata15@gmail.com>
2023-11-27 16:24:52 +0200

    fix wrappers

--- src/utils/LoginWrapper.jsx
+++ src/utils/LoginWrapper.jsx
@@ -1,18 +1,12 @@
 import React, {useContext} from "react";
-import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 
 
 export const LoginWrapper = () => {
-  const navigate = useNavigate()
   const {user} = useContext(AuthContext)
-  console.log(user)
-  if (!user) {
-    return <Outlet />;
-  } else {
-    navigate("/");
-    return null;
-  }
+  return !user ? <Outlet /> : <Navigate to="/" />;
+
 };
 
 
--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -1,14 +1,8 @@
 import React, { useContext } from "react";
-import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 
 export const PrivateWrapper = () => {
-  const navigate = useNavigate()
   const { user } = useContext(AuthContext);
-  if (user) {
-    return <Outlet />;
-  } else {
-    navigate("/login");
-    return null;
-  }
+  return user ? <Outlet /> : <Navigate to="/login" />;
 };
